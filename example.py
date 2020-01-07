bcafd6d06acabc1729c647c5464eb5118dedcd6e
XENIA <lommy101@gmail.com>
2020-01-08 05:41:20 +0900

    Update example.py

--- example.py
+++ example.py
@@ -10,7 +10,22 @@ def readjson():
         df = pd.DataFrame(list(json_data.items()),columns = ['ds','y']) 
         df['ds'] = pd.to_datetime(df['ds'])
         return df
+    
+def prophet(data):
+        model = Prophet()
+        model.fit(data)
 
+        future = model.make_future_dataframe(periods=365)
+        forecast = model.predict(future)
+        forecast.tail()
+
+        fig1 = model.plot(forecast)
+        fig2 = model.plot_components(forecast)
+
+        add_changepoints_to_plot(fig1.gca(), model, forecast, threshold=0)
+        
+        plt.show()
+        
 if __name__ == "__main__":
     data = readjson()
-
+    prophet(data)
